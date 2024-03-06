fc0549171213c6f0bbb2f051cf12ba5b8cc943ea
Vitalii Kharchenko <sir.vetal98@gmail.com>
2024-03-06 13:06:17 +0200

    Implement FileUploadService

---
+++ FileStorage.WebApp/Components/Services/FileUploadService.cs
@@ -0,0 +1,74 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using FileStorage.WebApp.Components.Dtos;
+using FluentValidation;
+
+namespace FileStorage.WebApp.Components.Services;
+
+public class FileUploadService : IFileUploadService
+{
+    private readonly BlobContainerClient _blobContainerClient;
+    private readonly IValidator<FileUploadRequest> _formDataValidator;
+    private readonly ILogger _logger;
+
+    public FileUploadService(BlobContainerClient blobContainerClient,
+        IValidator<FileUploadRequest> formDataValidator,
+        ILogger logger)
+    {
+        _blobContainerClient = blobContainerClient;
+        _formDataValidator = formDataValidator;
+        _logger = logger;
+    }
+    public async Task<FileUploadResponse> UploadAsync(FileUploadRequest request)
+    {
+        var uploadFileResponse = new FileUploadResponse();
+
+        await ValidateRequest(request, uploadFileResponse);
+        if (!uploadFileResponse.Success)
+        {
+            return uploadFileResponse;
+        }
+
+        BlobClient client = _blobContainerClient.GetBlobClient(request.File.Name);
+        IDictionary<string, string> metadata = new Dictionary<string, string>
+            {
+                { "Email", request.Email }
+            };
+        try
+        {
+            await using Stream data = request.File.OpenReadStream();
+            var response = await client.UploadAsync(data, new BlobUploadOptions
+            {
+                HttpHeaders = new BlobHttpHeaders { ContentType = "application/octet-stream" },
+                Metadata = metadata
+            });
+
+            uploadFileResponse.Url = client.Uri.AbsoluteUri;
+            uploadFileResponse.Success = true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occured during writting file to Blob Container");
+            uploadFileResponse.Errors.Add("Internal error");
+            uploadFileResponse.Success = false;
+        }
+
+        return uploadFileResponse;
+
+    }
+
+    private async Task<FileUploadResponse> ValidateRequest(FileUploadRequest request, FileUploadResponse response)
+    {
+        var result = await _formDataValidator.ValidateAsync(request);
+
+        if (!result.IsValid)
+        {
+            response.Success = true;
+            foreach (var error in result.Errors)
+            {
+                response.Errors.Add($"Property: {error.PropertyName}, Error: {error.ErrorMessage}");
+            }
+        }
+        return response;
+    }
+}
---
+++ FileStorage.WebApp/Components/Services/IFileUploadService.cs
@@ -0,0 +1,8 @@
+﻿using FileStorage.WebApp.Components.Dtos;
+
+namespace FileStorage.WebApp.Components.Services;
+
+public interface IFileUploadService
+{
+    Task<FileUploadResponse> UploadAsync(FileUploadRequest request);
+}
