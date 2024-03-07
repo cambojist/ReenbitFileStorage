f252d18d27ae775174352fdf7b362c4c719eb6ed
Vitalii Kharchenko <sir.vetal98@gmail.com>
2024-03-07 18:23:39 +0200

    #2 Smtp server now use enviromental variables

--- FileStorage.BlobFunction/Services/SMTPService.cs
+++ FileStorage.BlobFunction/Services/SMTPService.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using System.Net;
+﻿using System.Net;
 using System.Net.Mail;
 
 namespace FileStorage.BlobFunction.Services;
@@ -11,12 +10,12 @@ public class SMTPService : ISMTPService
     private readonly string _smtpHost;
     private readonly int _smtpPort;
 
-    public SMTPService(IConfiguration config)
+    public SMTPService()
     {
-        _fromEmail = config["FromEmail"];
-        _password = config["Password"];
-        _smtpHost = config["Host"];
-        _smtpPort = config.GetValue<int>("Port");
+        _fromEmail = Environment.GetEnvironmentVariable(EnviromentalConstant.FROM_EMAIL);
+        _password = Environment.GetEnvironmentVariable(EnviromentalConstant.PASSWORD);
+        _smtpHost = Environment.GetEnvironmentVariable(EnviromentalConstant.HOST);
+        _smtpPort = int.Parse(Environment.GetEnvironmentVariable(EnviromentalConstant.PORT));
     }
 
     public async Task SendEmailAsync(string toEmail, string link)
@@ -29,7 +28,7 @@ public class SMTPService : ISMTPService
         var mailMessage = new MailMessage(_fromEmail, toEmail)
         {
             Subject = "File Storage Service",
-            Body = $"Here is your link to file uploaded to File Storage: {link}"
+            Body = $@"Here is your link to uploaded file: <a href=""{link}"">Document</a>"
         };
 
         await client.SendMailAsync(mailMessage);
