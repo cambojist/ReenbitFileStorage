ba03c37ac2ec575ea85f9381bd59f45ba501dd2f
Vitalii Kharchenko <sir.vetal98@gmail.com>
2024-03-07 10:37:22 +0200

    init azure function

---
+++ FileStorage.BlobFunction/TriggerFunction.cs
@@ -0,0 +1,22 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace FileStorage.BlobFunction;
+
+public class TriggerFunction
+{
+    private readonly ILogger<TriggerFunction> _logger;
+
+    public TriggerFunction(ILogger<TriggerFunction> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(TriggerFunction))]
+    public async Task Run([BlobTrigger("reenbittask/{name}", Connection = "AzureWebJobsStorage")] Stream stream, string name)
+    {
+        using var blobStreamReader = new StreamReader(stream);
+        var content = await blobStreamReader.ReadToEndAsync();
+        _logger.LogInformation($"C# Blob trigger function Processed blob\n Name: {name} \n Data: {content}");
+    }
+}
