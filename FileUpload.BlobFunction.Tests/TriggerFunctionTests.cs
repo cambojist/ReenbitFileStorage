0b692a0af93307f915319e866fec0eb9defc90e4
Vitalii Kharchenko <sir.vetal98@gmail.com>
2024-03-08 21:03:03 +0200

    #2 Adjust unit test and refactor

--- FileUpload.BlobFunction.Tests/TriggerFunctionTests.cs
+++ FileUpload.BlobFunction.Tests/TriggerFunctionTests.cs
@@ -10,6 +10,10 @@ namespace FileStorage.Tests;
 
 public class TriggerFunctionTests
 {
+    private const string FILE_NAME = "testBlob.docx";
+    private const string EMAIL = "test@example.com";
+    private const string FAKE_SAS_TOKEN = "https://fake.sas.token";
+
     [Fact]
     public async Task Run_TriggerFunction_ProcessesBlob_AndSendsEmail()
     {
@@ -17,33 +21,44 @@ public class TriggerFunctionTests
         var logger = new Mock<ILogger<TriggerFunction>>();
         var sasTokenService = new Mock<ISASTokenService>();
         var smtpService = new Mock<ISMTPService>();
+        var client = new Mock<BlobClient>();
+        var responseMock = new Mock<Response>();
+
+        sasTokenService
+            .Setup(s => s.GetTokenAsync(FILE_NAME))
+            .Returns(Task.FromResult(FAKE_SAS_TOKEN));
 
         var mdata = new Dictionary<string, string>
             {
-                { "Email", "test@example.com" }
+                { "Email", EMAIL }
             };
-
         var blobProps = BlobsModelFactory.BlobProperties(metadata: mdata);
         var result = BlobsModelFactory.BlobDownloadResult(content: null);
-
-        var client = new Mock<BlobClient>();
-        var responseMock = new Mock<Response>();
         client
-            .Setup(m => m.GetPropertiesAsync(null, CancellationToken.None).Result)
-            .Returns(Response.FromValue(blobProps, responseMock.Object));
+            .Setup(m => m.GetPropertiesAsync(null, CancellationToken.None))
+            .Returns(Task.FromResult(Response.FromValue(blobProps, responseMock.Object)));
 
         var function = new TriggerFunction(logger.Object, sasTokenService.Object, smtpService.Object);
 
         // Act
-        await function.Run(client.Object, "testBlob");
+        await function.Run(client.Object, FILE_NAME);
 
         // Assert
 
         logger.Verify(
-                x => x.Log(LogLevel.Information, It.IsAny<EventId>(), It.Is<It.IsAnyType>((v, t) => v.ToString().Contains("Sent email to: test@example.com")), It.IsAny<Exception>(), (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()),
+                x => x.Log(LogLevel.Information, It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Contains($"Blob trigger function processed blob\n Name:{FILE_NAME}")),
+                It.IsAny<Exception>(), (Func<It.IsAnyType, Exception?, string>)It.IsAny<object>()),
                 Times.Once);
-        //await sasTokenService.Received(1).GetTokenAsync("testBlob");
-        //await smtpService.Received(1).SendEmailAsync("test@example.com", Arg.Any<string>());
-        //logger.Received(1).LogInformation("Error");
+
+        sasTokenService.Verify(s => s.GetTokenAsync(FILE_NAME), Times.Once);
+
+        smtpService.Verify(s => s.SendEmailAsync(EMAIL, FAKE_SAS_TOKEN), Times.Once);
+
+        logger.Verify(
+                x => x.Log(LogLevel.Information, It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Contains($"Sent email to: {EMAIL}")),
+                It.IsAny<Exception>(), (Func<It.IsAnyType, Exception?, string>)It.IsAny<object>()),
+                Times.Once);
     }
 }
